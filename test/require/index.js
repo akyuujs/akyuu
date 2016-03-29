3c5ccf4a13b00e54689044934ee5bde239c6bcb4
XadillaX <admin@xcoder.in>
2016-03-29 17:15:47 +0800

    add test for require

---
+++ test/require/index.js
@@ -0,0 +1,37 @@
+/**
+ * XadillaX created at 2016-03-23 16:05:01 With ♥
+ *
+ * Copyright (c) 2016 Souche.com, all rights
+ * reserved.
+ */
+"use strict";
+
+var req = require("../../lib/require");
+
+describe("require", function() {
+    before(function() {
+        req.addRequirePath(__dirname);
+    });
+
+    it("should add require path", function() {
+        req.addRequirePath(__dirname + "/a");
+        req.paths.length.should.be.eql(2);
+        req.paths[1].should.be.eql(__dirname + "/a");
+    });
+
+    it("should remove require path", function() {
+        req.removeRequirePath(__dirname + "/a");
+        req.paths.length.should.be.eql(1);
+        req.paths[0].should.be.eql(__dirname);
+    });
+
+    describe("let's require", function() {
+        it("should require test_require.js", function() {
+            var test = require("test_require");
+            test.should.be.eql(__dirname);
+        });
+
+        it("should require ./test_require.js", function() {
+        });
+    });
+});
---
+++ test/require/test_require.js
@@ -0,0 +1,9 @@
+/**
+ * XadillaX created at 2016-03-23 16:09:32 With ♥
+ *
+ * Copyright (c) 2016 Souche.com, all rights
+ * reserved.
+ */
+"use strict";
+
+module.exports = __dirname;
