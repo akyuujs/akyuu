b475a7309f1e4a083e791b59fa8a1ce110ff6ffd
XadillaX <admin@xcoder.in>
2016-04-19 16:39:53 +0800

    modify akyuu creator method

--- index.js
+++ index.js
@@ -6,7 +6,41 @@
  */
 "use strict";
 
-var Akyuu = module.exports = require("./lib/akyuu");
+require("sugar");
+
+var _defaultAkyuu = null;
+var _akyuues = {};
+
+var Akyuu = module.exports = {};
+
+/**
+ * create
+ * @param {String} root the project root path for Akyuu
+ * @param {String} [name] this akyuu instance name
+ * @returns {Akyuu} the akyuu object
+ */
+Akyuu.create = function(root, name) {
+    var akyuu = new (require("./lib/akyuu"))(root);
+
+    if(name) _akyuues[name] = akyuu;
+    else {
+        _defaultAkyuu = akyuu;
+    }
+
+    return akyuu;
+};
+
+/**
+ * get
+ * @param {String} [name] the akyuu instance name
+ * @returns {Akyuu} the akyuu object
+ */
+Akyuu.get = function(name) {
+    if(!name) return _defaultAkyuu;
+    else {
+        return _akyuues[name];
+    }
+};
 
 Akyuu.Joi = require("joi");
 
