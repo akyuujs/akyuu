dff26c3d688875380159bc1ba5bb256f72a6ab5e
XadillaX <admin@xcoder.in>
2016-05-18 18:30:41 +0800

    enable json-cuoutput plugin in demo

---
+++ example/demo/src/config/default/plugins.js
@@ -0,0 +1,14 @@
+/**
+ * XadillaX created at 2016-05-14 18:04:26 With ♥
+ *
+ * Copyright (c) 2016 Souche.com, all rights
+ * reserved.
+ */
+"use strict";
+
+module.exports = {
+    jsonCuoutput: {
+        enabled: true,
+        output: "underscore"
+    }
+};
