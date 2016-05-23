4bfdb3e720ac81426915c06f71fcf47e4cd898fb
XadillaX <admin@xcoder.in>
2016-05-23 11:03:53 +0800

    override console

--- example/demo/src/config/local/logger.js
+++ example/demo/src/config/local/logger.js
@@ -1,10 +1,11 @@
 "use strict";
 
-module.exports = [
-    {
+module.exports = {
+    overrideConsole: true,
+    transports: [{
         type: "file",
         json: false,
         filename: "akyuu.log",
         formatter: "default"
-    }
-];
+    }]
+};
