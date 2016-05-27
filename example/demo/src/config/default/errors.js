6a5c2abcf5147a902e303bcfe0c973697cb5b561
XadillaX <admin@xcoder.in>
2016-05-27 14:17:51 +0800

    add demo errors config

---
+++ example/demo/src/config/default/errors.js
@@ -0,0 +1,15 @@
+/**
+ * XadillaX created at 2016-06-01 13:52:05 With ♥
+ *
+ * Copyright (c) 2016 Souche.com, all rights
+ * reserved.
+ */
+"use strict";
+
+module.exports = {
+    Example: {
+        code: 100,
+        status: 200,
+        business: "this is an example error."
+    }
+};
