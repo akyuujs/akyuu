fdc18a7d89ae1f5e30455ff5b023158868143472
XadillaX <admin@xcoder.in>
2017-04-13 15:04:45 +0800

    chore(eslint): update .eslintrc

--- test/require/index.js
+++ test/require/index.js
@@ -6,7 +6,7 @@
  */
 "use strict";
 
-var req = require("../../lib/require");
+const req = require("../../lib/require");
 
 describe("require", function() {
     before(function() {
@@ -14,20 +14,20 @@ describe("require", function() {
     });
 
     it("should add require path", function() {
-        req.addRequirePath(__dirname + "/a");
+        req.addRequirePath(`${__dirname}/a`);
         req.paths.length.should.be.eql(2);
-        req.paths[1].should.be.eql(__dirname + "/a");
+        req.paths[1].should.be.eql(`${__dirname}/a`);
     });
 
     it("should remove require path", function() {
-        req.removeRequirePath(__dirname + "/a");
+        req.removeRequirePath(`${__dirname}/a`);
         req.paths.length.should.be.eql(1);
         req.paths[0].should.be.eql(__dirname);
     });
 
     describe("let's require", function() {
         it("should require test_require.js", function() {
-            var test = require("test_require");
+            const test = require("test_require");
             test.should.be.eql(__dirname);
         });
 
