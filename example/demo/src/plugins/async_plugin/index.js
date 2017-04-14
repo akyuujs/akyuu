9f1fd204ce4177c349e5361e69793376f60e510c
mapleincode <llkj13579@126.com>
2017-04-14 13:07:57 +0800

    chore(example): add thired async plugin demo

---
+++ example/demo/src/plugins/async_plugin/index.js
@@ -0,0 +1,26 @@
+class AsyncPlugin {
+    constructor(akyuu) {
+        this.akyuu = akyuu;
+        this.position = akyuu.PLUGIN_POS.BEFORE_CONTROLLER;
+    }
+
+    plug(callback) {
+        const self = this;
+        setTimeout(function() {
+            self.akyuu.get('/async_plugin', function(req, resp) {
+                resp.succ('success-load-sync-plugin');
+            });
+            callback();
+        }, 500)
+    }
+}
+
+/**
+ * create
+ * @param akyuu
+ * @param options
+ * @returns {AsyncPlugin}
+ */
+exports.create = function(akyuu, options) {
+    return new AsyncPlugin(akyuu, options);
+};
